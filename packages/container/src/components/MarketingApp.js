e1b4469c27013786269c47944bf5093f6e4ffa95
iosipciuc <ion-osipciuc@mail.ru>
2023-03-31 17:02:34 +0300

    fix

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,16 +1,18 @@
-import { mount } from 'marketing/MarketingApp'
+// import { mount } from 'marketing/MarketingApp'
 import React, {useEffect, useRef} from 'react'
+import('marketing/MarketingApp').then(({mount}) => {
+  const MarketingApp = () => {
+    const ref = useRef(null)
 
-const MarketingApp = () => {
-  const ref = useRef(null)
+    useEffect(() => {
+      mount(ref.current)
+    }, [])
 
-  useEffect(() => {
-    mount(ref.current)
-  }, [])
+    return (
+      <div ref={ref} />
+    )
+  }
 
-  return (
-    <div ref={ref} />
-  )
-}
+  export default MarketingApp
 
-export default MarketingApp
+});
