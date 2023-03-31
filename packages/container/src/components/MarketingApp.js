6a594e920365a4ad16b15544a2291f1189ddfdc4
iosipciuc <ion-osipciuc@mail.ru>
2023-03-31 17:27:32 +0300

    fix

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -14,6 +14,7 @@ const MarketingApp = () => {
   useEffect(() => {
     fetchComponent()
       .then(callback => {
+        console.log(callback)
         callback.mount(ref.current)
       })
       .catch(e => {
