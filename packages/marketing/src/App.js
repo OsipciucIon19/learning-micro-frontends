c7f8e4724a39264ea13be85e26dc3f2e297d7265
iosipciuc <ion-osipciuc@mail.ru>
2023-04-03 11:07:59 +0300

    fix css namespace

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import {StylesProvider} from "@material-ui/core";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core";
 import Pricing from "./components/Pricing"
 import Landing from "./components/Landing"
-const App = (props) => {
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
+const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
