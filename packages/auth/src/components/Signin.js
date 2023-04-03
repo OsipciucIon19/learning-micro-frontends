885e548a5dcf235633599e60877fb030932bdb0a
iosipciuc <ion-osipciuc@mail.ru>
2023-04-03 16:19:14 +0300

    add auth pipeline action

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -15,7 +15,7 @@ import { Link } from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © 2023'}
+      {'Copyright © '}
       <Link color="inherit" to="/">
         Your Website
       </Link>{' '}
