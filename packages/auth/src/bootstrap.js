eb9020850af451aef42cdfee300cd251e86889f3
iosipciuc <ion-osipciuc@mail.ru>
2023-04-04 15:21:05 +0300

    add auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import {render} from "react-dom";
 import App from "./App";
 import {createMemoryHistory, createBrowserHistory} from 'history'
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, onSignIn, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath]
   })
@@ -12,7 +12,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     history.listen(onNavigate)
   }
 
-  render(<App history={history} />, el)
+  render(<App onSignIn={onSignIn} history={history} />, el)
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, {lazy, Suspense, useState} from 'react'
 import Header from "./components/Header";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {StylesProvider, createGenerateClassName} from "@material-ui/core";
@@ -12,13 +12,20 @@ const generateClassName = createGenerateClassName({
 })
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
-        <Header/>
+        <Header
+          isSignedIn={isSignedIn}
+          onSignOut={() => setIsSignedIn(false)}
+        />
         <Suspense fallback={<Progress />}>
           <Switch>
-            <Route path="/auth" component={AuthLazy}/>
+            <Route path="/auth">
+              <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+            </Route>
             <Route path="/" component={MarketingLazy}/>
           </Switch>
         </Suspense>
