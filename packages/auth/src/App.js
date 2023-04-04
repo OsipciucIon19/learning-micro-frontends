4b70e0a66e61668be7a7f6fc0f053a564dd0641a
iosipciuc <ion-osipciuc@mail.ru>
2023-04-04 15:28:11 +0300

    add auth

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Switch, Route, Router} from 'react-router-dom'
 import {StylesProvider, createGenerateClassName} from "@material-ui/core";
 import SignUp from "./components/Signup";
